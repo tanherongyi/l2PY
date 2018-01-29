32f56d35c669a7fe0b5224c23d002747a46db7aa
tanherongyi <94112776@qq.com>
2018-01-30 01:10:07 +0800

    文章增加打赏功能

--- app/some_func.py
+++ app/some_func.py
@@ -92,6 +92,36 @@ class OAuthMethod():
         result = r.json()
         return result
 
+    def weibo_nomal_path(self):
+        path = current_app.config['WEIBO_AUTHORIZE_URL'] + '?client_id=' + current_app.config[
+            'WEIBO_CLIENT_ID'] + '&response_type=code' + '&redirect_uri=' + 'http://www.l2py.com/weibo/oauth/callback'
+        return path
+
+    def weibo_nomal_userinfo(self):
+        # 判断回调地址中是否拿到code
+        if 'code' not in request.args:
+            return redirect(url_for('weibo_login'))
+        code = request.args.get('code')
+
+        # 通过传入client_id、client_secret、code三个参数向 'https://api.weibo.com/oauth2/access_token' 发送POST请求，获取access_token
+        url = 'https://api.weibo.com/oauth2/access_token'
+        params = {
+            'client_id': current_app.config['WEIBO_CLIENT_ID'],
+            'client_secret': current_app.config['WEIBO_CLIENT_SECRET'],
+            'code': code
+        }
+        r = requests.post(url, params=params)
+        r.encoding = r.content
+        result = r.json()
+        access_token = result['access_token']
+
+        # 将access_token 传入'https://api.weibo.com/users/show?access_token='，发送GET请求，获取用户信息
+        url = 'https://api.weibo.com/users/show?access_token=%s' % access_token
+        r = requests.get(url)
+        r.encoding = r.content
+        result = r.json()
+        return result
+
 def ch_content(content):
     ch_rule = re.compile(r'<[^>]+>', re.S)
     ch_content = ch_rule.sub('', content)
